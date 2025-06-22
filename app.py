1db7d81ef848f9a10d2ad25fa2568e725dc5b18c
bunnyyy122 <bunnyyyisalie@gmail.com>
2025-06-22 02:08:30 +0000

    ho jaye

---
+++ app.py
@@ -0,0 +1,61 @@
+from flask import Flask, request, render_template, redirect, session
+from sqlalchemy import create_engine, text
+
+app = Flask(__name__)
+app.secret_key = "something-very-secret"
+
+engine = create_engine(
+    "mysql+pymysql://root:tsDnHFAhmftFWhJFZFMeMwDkazULlMTQ@shortline.proxy.rlwy.net:55403/railway?charset=utf8mb4"
+)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def homepage():
+    return render_template('home.html')
+
+
+@app.route('/login/<subject>', methods=['GET', 'POST'])
+def login(subject):
+        if request.method == 'POST':
+            username = request.form['username']
+            password = request.form['password']
+
+            with engine.connect() as conn:
+                result = conn.execute(
+                    text(f"SELECT * FROM {subject} WHERE us = :u AND ps = :p"),
+                    {'u': username, 'p': password}
+                )
+                user = result.fetchone()
+
+            if user:
+                session['username'] = username
+                return render_template(f"{subject}.html")
+            else:
+                return 'Invalid login'
+
+        return render_template('login1.html', subject=subject)
+
+
+@app.route('/eco.html', methods=['GET', 'POST'])
+def eco():
+    return render_template('eco.html')
+    
+@app.route('/bcom.html', methods=['GET', 'POST'])
+def bcom():
+    return render_template('bcom.html')
+    
+@app.route('/bba.html', methods=['GET', 'POST'])
+def bba():
+    return render_template('bba.html')
+
+@app.route('/pol.html', methods=['GET', 'POST'])
+def pol():
+    return render_template('pol.html')
+
+@app.route('/bms.html', methods=['GET', 'POST'])
+def bms():
+    return render_template('bms.html')
+    
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
+    # app.run(debug=True)
